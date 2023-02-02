7b6895ded0e59174df1d82271dc053ef19ac3d80
Tanner Byers <naultic@amazon.com>
2023-02-02 09:46:33 -0500

    removed placeholder text and added gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b575ab8a7e47c246"; }
+/******/ 		__webpack_require__.h = function() { return "4915fdf3df955d0e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
